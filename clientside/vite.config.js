61e4c66463216efffc8f6f451bcc8af932da1ac3
mhd-sulu-786 <muhammedsulaimant162@gmail.com>
2025-04-30 02:01:36 +0530

    updated path issue vite configed the name

--- clientside/vite.config.js
+++ clientside/vite.config.js
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TrendyBazarr/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
